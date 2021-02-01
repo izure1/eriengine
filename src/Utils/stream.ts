72577f9ba2003f5e1631d2f40bb2ecc2435e1745
izure <=>
2021-02-01 11:53:19 +0900

    update

--- src/Utils/stream.ts
+++ src/Utils/stream.ts
@@ -1,9 +1,12 @@
-import { ipcMain, ipcRenderer, BrowserWindow, IpcRendererEvent } from 'electron'
+import { ipcMain, ipcRenderer, BrowserWindow, IpcRendererEvent, IpcMainEvent } from 'electron'
 import Stream from 'stream'
 
 
 const STREAM_IPC_CHANNEL: string = '__eriengine-standard-ipc-stream-channel__'
 
+/**
+ * Electron background에서 Renderer로 IPC를 이용한 데이터를 전송하기 위한 스트림 클래스입니다
+ */
 class MainWriteStream extends Stream.Writable {
     protected channel: string
     private win: BrowserWindow|null = null
@@ -30,6 +33,56 @@ class MainWriteStream extends Stream.Writable {
     }
 }
 
+/**
+ * Electron Renderer에서 background로 IPC를 이용한 데이터를 전송하기 위한 스트림 클래스입니다
+ */
+class RendererWriteStream extends Stream.Writable {
+    protected channel: string
+    
+    constructor(channel: string) {
+        super()
+        this.channel = channel
+    }
+
+    _write(chunk: Buffer|string|any, encoding: string, callback: (err?: Error|null) => void): void {
+        ipcRenderer.send(STREAM_IPC_CHANNEL, { channel: this.channel, data: chunk.toString() })
+        callback(null)
+    }
+
+    _destroy(error: Error|null, callback: (error?: Error|null) => void): void {
+        callback(null)
+    }
+}
+
+/**
+ * Electron background에서 Renderer로부터 IPC로 전송된 데이터를 수신받는 스트림 클래스입니다
+ */
+class MainReadStream extends Stream.Readable {
+    protected channel: string
+    private onReceive = (e: IpcMainEvent, chunk: Engine.Type.StreamChunk): void => {
+        if (chunk.channel !== this.channel) {
+            return
+        }
+        this.push(chunk.data)
+    }
+
+    constructor(channel: string) {
+        super()
+        this.channel = channel
+        ipcMain.on(STREAM_IPC_CHANNEL, this.onReceive)
+    }
+
+    _read(size: number): void {}
+
+    _destroy(error: Error|null, callback: (error?: Error|null) => void): void {
+        ipcMain.off(STREAM_IPC_CHANNEL, this.onReceive)
+        callback(null)
+    }
+}
+
+/**
+ * Electron Renderer에서 background로부터 IPC로 전송된 데이터를 수신받는 스트림 클래스입니다
+ */
 class RendererReadStream extends Stream.Readable {
     protected channel: string
     private onReceive = (e: IpcRendererEvent, chunk: Engine.Type.StreamChunk): void => {
@@ -53,10 +106,34 @@ class RendererReadStream extends Stream.Readable {
     }
 }
 
-export function writeToRenderer(channel: string): Stream.Writable {
+/**
+ * 백그라운드에서 렌더러로 데이터를 전송하는 스트림을 생성합니다
+ * @param channel 전송할 채널입니다
+ */
+export function writeToRenderer(channel: string): MainWriteStream {
     return new MainWriteStream(channel)
 }
 
-export function readFromMain(channel: string): Stream.Readable {
+/**
+ * 렌더러에서 백그라운드로 데이터를 전송하는 스트림을 생성합니다
+ * @param channel 전송할 채널입니다
+ */
+export function writeToMain(channel: string): RendererWriteStream {
+    return new RendererWriteStream(channel)
+}
+
+/**
+ * 렌더러에서 전송된 데이터를 수신받을 스트림을 생성합니다
+ * @param channel 수신받을 채널입니다
+ */
+export function readFromRenderer(channel: string): MainReadStream {
+    return new MainReadStream(channel)
+}
+
+/**
+ * 백그라운드에서 전송된 데이터를 수신받을 스트림을 생성합니다
+ * @param channel 수신받을 채널입니다
+ */
+export function readFromMain(channel: string): RendererReadStream {
     return new RendererReadStream(channel)
 }
